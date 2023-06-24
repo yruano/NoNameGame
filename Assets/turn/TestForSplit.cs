1d522321afa1ee1dfa379081e7d5023d60f03c3a
ilkongil <kongmin74@gmail.com>
2023-06-24 20:58:22 +0900

    test for branch split
    이게 제대로 되는건지 확인하려고

---
+++ Assets/turn/TestForSplit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForSplit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //브랜치인지 브런치인지 나뉘는지 확인용파일
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
