a2bd486be7547dbe460dc69230094887f8a6458f
yruano <51043000+yruano@users.noreply.github.com>
2023-06-30 22:19:32 +0900

    추가

---
+++ Assets/Game/Card/Card.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class Card : MonoBehaviour
+{
+  [SerializeField] SpriteRenderer card;
+  [SerializeField] SpriteRenderer character;
+  [SerializeField] TMP_Text nameTMP;
+  [SerializeField] TMP_Text attackTMP;
+  [SerializeField] TMP_Text actionTMP;
+
+  public CardItem carditem;
+  public PRS originPRS;
+
+  public void Setup(CardItem carditem)
+  {
+    this.carditem = carditem;
+    character.sprite = this.carditem.sprite;
+    nameTMP.text = this.carditem.name;
+    attackTMP.text = this.carditem.attack.ToString();
+    actionTMP.text = this.carditem.action.ToString();
+  }
+
+  public void MoveTransform(PRS prs, bool useDotween, float dotweenTime = 0)
+  {
+    if (useDotween)
+    {
+      transform.DOMove(prs.pos, dotweenTime);
+      transform.DORotateQuaternion(prs.rot, dotweenTime);
+      transform.DOScale(prs.scale, dotweenTime);
+    }
+    else
+    {
+      transform.position = prs.pos;
+      transform.rotation = prs.rot;
+      transform.localScale = prs.scale;
+    }
+  }
+
+  void OnMouseOver()
+  {
+    CardManager.Inst.CardMouseOver(this);
+  }
+
+  void OnMouseExit()
+  {
+    CardManager.Inst.CardMouseExit(this);
+  }
+
+  void OnMouseDown()
+  {
+    CardManager.Inst.CardMouseDown();
+  }
+
+  void OnMouseUp()
+  {
+    CardManager.Inst.CardMouseUp();
+  }
+}
---
+++ Assets/Game/Card/Utils.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 카드 원본 위치
+[System.Serializable]
+public class PRS
+{
+  public Vector3 pos;
+  public Quaternion rot;
+  public Vector3 scale;
+
+  public PRS(Vector3 pos, Quaternion rot, Vector3 scale)
+  {
+    this.pos = pos;
+    this.rot = rot;
+    this.scale = scale;
+  }
+}
+
+public class Utils
+{
+  public static Quaternion QI => Quaternion.identity;
+
+  public static Vector3 MousePos
+  {
+    get
+    {
+      Vector3 result = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      result.z = -10;
+      return result;
+    }
+  }
+}
