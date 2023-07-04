94af8692fa11cabd57b5501a234a32dc81326fff
ilkongil <kongmin74@gmail.com>
2023-07-05 00:00:28 +0900

    skill스크립트 삭제
    skillData스크립트로 대체및 관련 구조 수정

--- Assets/turn/turnMonster/Monster/skill/SkillData.cs
+++ Assets/turn/turnMonster/Monster/skill/SkillData.cs
@@ -8,17 +8,31 @@ using static UnityEngine.GraphicsBuffer;
 using System.Linq;
 //구글 시트에서 모든 데이터를 가져와 double배열로 갖고 저장합니다.
 
-[CreateAssetMenu(fileName = "LoadSkillData", menuName = "Scriptable Object/LoadSkillData_Test")]
-public class LoadSkillData : ScriptableObject
+[CreateAssetMenu(fileName = "SkillData", menuName = "Scriptable Object/SkillData_LoadTest")]
+public class SkillData : ScriptableObject
 {
     [SerializeField]
     public string associatedSheet = "1mpe5Gjq7nO5HTYhLEZkTnNSrFAMFnCKSGZ0tyuJynds";
     public string associatedWorksheet = "test";
     public List<List<double>> Skills = new List<List<double>>();
     public Dictionary<string, int> NameToId = new Dictionary<string, int>();
-    //실제로 데이터 불러오는 부분
+    public Dictionary<int, string> IdToName = new Dictionary<int, string>();
 
-    //데이터 로드
+    //공통 인덱스
+    public const int ID = 0;
+    public const int PRIORITY = 1;
+    //공격스킬 인덱스
+    public const int DAMAGE = 2;
+    public const int HIT_COUNT = 3;
+    //버프스킬 인덱스
+    public const int ADD_VAL = 2;
+    public const int MULTI_VAL = 3;
+    public const int TARGET_STAT = 4;
+    public const int DURATION = 5;
+    //힐 스킬 인덱스
+    public const int HEAL_AMOUNT = 2;
+
+    //실제로 데이터 불러오는 부분
     private void Awake()
     {
         LoadDataCall(ProcessingData);
@@ -43,8 +57,11 @@ public class LoadSkillData : ScriptableObject
             Skills[Skills.Count - 1].Add(int.Parse(ss.rows[rows][0].value));
             //우선순위
             Skills[Skills.Count - 1].Add(int.Parse(ss.rows[rows][1].value));
+            //이름과 ID 연결
+            NameToId[ss.rows[rows][2].value] = (int)Skills[Skills.Count - 1][0];
+            IdToName[(int)Skills[Skills.Count - 1][0]] = ss.rows[rows][2].value;
 
-            //공격 데이터를 읽는 중 이라면 
+            //공격 데이터를 읽는 부분
             if ((int)(Skills[Skills.Count - 1][0]) % 10 == 1)
             {
                 //데미지
@@ -52,7 +69,7 @@ public class LoadSkillData : ScriptableObject
                 //타수
                 Skills[Skills.Count - 1].Add(int.Parse(ss.rows[rows][8].value));
             }
-            //버프데이터를 읽는 중 이라면
+            //버프데이터를 읽는 부분
             else if ((int)(Skills[Skills.Count - 1][0]) % 10 == 2 || (int)(Skills[Skills.Count - 1][0]) % 10 == 3)
             {
                 //덧셈 값
@@ -64,7 +81,7 @@ public class LoadSkillData : ScriptableObject
                 //지속 시간
                 Skills[Skills.Count - 1].Add(int.Parse(ss.rows[rows][7].value));
             }
-            //힐 데이터를 읽는 중 이라면 
+            //힐 데이터를 읽는 부분
             else if ((int)(Skills[Skills.Count - 1][0]) % 10 == 4)
             {
                 //힐량
@@ -72,22 +89,24 @@ public class LoadSkillData : ScriptableObject
 
             }
             //로그남기기, string.Join은 list내부 데이터를 한개의 string으로 묶는것
-            Debug.Log("데이터 불러옴: " + string.Join(", ", Skills[Skills.Count - 1].Select(d => d.ToString()).ToArray()));
+            Debug.Log("데이터 불러옴: " + string.Join(", ", Skills[Skills.Count - 1].Select(d => d.ToString()).ToArray()) + ": " + IdToName[(int)Skills[Skills.Count - 1][0]]);
         }
-        //-------------------------------------
 
+        Debug.Log("데이터 로딩 완료");
     }
 }
+
+//------
 //inspector창 구성
-[CustomEditor(typeof(LoadSkillData))]
+[CustomEditor(typeof(SkillData))]
 public class DataTest : Editor
 {
-    LoadSkillData data;
+    SkillData data;
 
     //초기화
     void OnEnable()
     {
-        data = (LoadSkillData)target;
+        data = (SkillData)target;
     }
 
     //버튼 구성
